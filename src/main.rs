4634817dff768f8d357465c35a7c4beff0df062a
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-05-31 10:45:56 +0200

    New wrap example with correct ip and port

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
+    println!("Setting up webserver.");
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .map(|name| format!("Hallo, {}!", name));
 
+    println!("Starting webserver at port 8000!");
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([0, 0, 0, 0], 8000))
         .await;
 }
