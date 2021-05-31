7e5dd30a1c1313fb5a77e71de662dd3e34ffbe13
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-05-31 11:20:46 +0200

    More than one endpoint

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
     println!("Setting up webserver.");
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hallo, {}!", name));
+    // GET /
+    let hello_world = warp::path::end().map(|| "Hello, World at root!");
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hallo, {}!", name));
+
+    let routes = warp::get().and(hello_world.or(hello));
 
     println!("Starting webserver at port 8000!");
-    warp::serve(hello)
-        .run(([0, 0, 0, 0], 8000))
-        .await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
